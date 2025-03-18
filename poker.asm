353206aa8a745a54c5019231c0acad7792cf498b
Alejandro Moltó <96083767+alejandromolto@users.noreply.github.com>
2025-03-18 14:24:33 +0100

    Update poker.asm
    Added a language menu, a "shown cards" array to keep track of the cards that have been shown, and two subrutines.

--- poker.asm
+++ poker.asm
@@ -6,21 +6,21 @@
 
 ## TEXT MESSAGES
 
-menu_mensaje : .asciiz "**BIENVENIDO A ASM-POKER** \n Trabajo hecho por alejandromolto. \n \n OPCIONES: \n \n (1) Jugar al poker. \n (2) Opciones. \n (3) Salida. \n \n Elige tu opción: "
+menu_mensaje : .asciiz "\n\n**BIENVENIDO A ASM-POKER** \n Trabajo hecho por alejandromolto. \n \n OPCIONES: \n \n (1) Jugar al poker. \n (2) Opciones. \n (3) Salida. \n \nElige tu opciÃ³n: "
+menu_message : .asciiz "\n\n**WELLCOME TO ASM-POKER** \n Work done by alejandromolto. \n \n OPTIONS: \n \n (1) Play poker. \n (2) Settings. \n (3) Exit. \n \n Choose your option: "
+opciones_mensaje: .asciiz "\n\n**SETTINGS MENU**\n\n(1) Change language\n(2) Exit\n \nElige tu opciÃ³n:"
+settings_message: .asciiz "\n\n**MENÃš DE CONFIGURACIÃ“N**\n\n(1) Cambiar idioma\n(2) Salir\n\nChoose your option: "
+lenguaje_mensaje: .asciiz "\n\n**MENÃš DE IDIOMA**\n\n(1) InglÃ©s\n(2) EspaÃ±ol \n(3) Salir\n\nElige tu opciÃ³n:"
+language_message: .asciiz "\n\n**LANGUAGE MENU**\n\n(1) English\n(2) Spanish \n(3) Exit\n\nChoose your option: "
+opcion_invalida: .asciiz  "\nPorfavor, elige una opciÃ³n vÃ¡lida: "
+invalid_option: .asciiz "\nPlease choose a valid option: "
+game_starting: .asciiz "The match is going to start."
+empezando_juego: .asciiz "La partida va a comenzar. El croupier estÃ¡ barajando las cartas..."
+
+
+## VALUES
 .align 2
-menu_message : .asciiz "**WELLCOME TO ASM-POKER** \n Work done by alejandromolto. \n \n OPTIONS: \n \n (1) Play poker. \n (2) Settings. \n (3) Exit. \n \n Choose your option: "
-.align 2
-opciones_mensaje: .asciiz "**SETTINGS MENU**\n\n(1) Change language\n(2) Exit\n"
-.align 2
-settings_message: .asciiz "**MENÚ DE CONFIGURACIÓN**\n\n(1) Cambiar idioma\n(2) Salir\n"
-.align 2
-lenguaje_memsaje: .asciiz "**MENÚ DE IDIOMA**\n\n(1) Español\n(2) Inglés \n(3) Salir"
-.align 2
-langauge_message: .asciiz "**LANGUAGE MENU**\n\n(1) Spanish\n(2) English \n(3) Exit\n"
-.align 2
-opcion_invalida: .asciiz  "Porfavor, elige una opción válida: "
-.align 2
-invalid_option: .asciiz "Please choose a valid option: "
+shownCards: .space 24  ## Array that stores the cards that already have been shonw
 
 
 ## MAIN
@@ -32,11 +32,12 @@ invalid_option: .asciiz "Please choose a valid option: "
 
 main:
 
-li $s3, 1
+li $s0, 1
 
 mainmenu:
 	
-	addi $t0, $s3, -1
+	## If language = 1, then the message displays in english. Any other value for s3 displays the message in espanish (esp1).
+	addi $t0, $s0, -1
 	bnez $t0, esp1
 	
 	la $a0, menu_message
@@ -67,15 +68,99 @@ mainmenu:
 	syscall
 	j ask_option	
 
-game:
 
 language_menu:
 
+	## If language = 1, then the message displays in english. Any other value for s3 displays the message in espanish (esp2).
+	addi $t0, $s0, -1
+	bnez $t0, esp2
 	
+	la $a0, language_message
+	li $v0, 4
+	syscall
+	j ask_language
+	
+	esp2:
+		la $a0, lenguaje_mensaje
+		li $v0, 4
+		syscall
+	
+	ask_language: 
+
+	li $v0, 5
+	syscall
+
+	li $t1, 1
+	li $t2, 2
+	li $t3, 3
+	
+	beq $v0, $t1, set_english
+	beq $v0, $t2, set_spanish
+	beq $v0, $t3, mainmenu 
+
+	la $a0 invalid_option
+	li $v0, 4
+	syscall
+	j ask_language	
+
+	set_english:
+		li $s0, 1
+		j mainmenu
+		
+	set_spanish:
+		li $s0, 2
+		j mainmenu
 
 
+game:
+
+	
 
 
 exit:
 	li $v0, 10
 	syscall
+
+
+## SUBRUTINES
+
+IsTheCardShown: ## This function receives a card in the paramater $a0 and returns to $v1 a 1 if it is has already been shown and a 0 if it hasnt been shown.
+
+li $v1, 0		# The default value is not shown
+li $t4, 6		# Number of times the loop has to be repeated
+li $t0, 0 		# Counter
+la $t1, shownCards	# Adress of the array
+
+	loop1:
+	lw $t2, 0($t1)			# Gets the value of the cards individually.
+	beq $t2, $a0, set_as_shown	# If the card has been shown it sets it as found and goes back to the main function.
+	beq $t0, $t4, back_to_main	# If the counter arrives to the maximum number of iterations it goes back the main.
+	addi $t1, $t1, 4		# The adress gets 4 bits added, moving on to the next card
+	addi $t0, $t0, 1		# The normal counter + 1
+	j loop1
+	
+	set_as_shown:
+		li $v1, 1
+	
+	back_to_main:
+		jr $ra
+
+
+SetCardsShownToZero: ## This function sets all the values of the array to 0.
+
+li $v1, 0		# The default value is not shown
+li $t4, 6		# Number of times the loop has to be repeated
+li $t0, 0 		# Counter
+la $t1, shownCards	# Adress of the array
+
+	loop2:
+	sw $zero, 0($t1)		# Erases the value of the cards individually.
+	beq $t0, $t4, back_to_main2	# If the counter arrives to the maximum number of iterations it goes back the main.
+	addi $t1, $t1, 4		# The adress gets 4 bits added, moving on to the next card
+	addi $t0, $t0, 1		# The normal counter + 1
+	j loop2
+
+	back_to_main2:
+		jr $ra
+
+
